e3e7883045cca70bc72304d4f36ad96a75f856d8
SarayutZ <wanoizloserBZ@gmail.com>
2023-08-09 14:52:19 +0700

    อยากรวย คูณ 2

--- src/components/test.vue
+++ src/components/test.vue
@@ -2,19 +2,15 @@
 
   <router-view />
   <div class="container">
-    <h1>หวัดดี พวก</h1>
+    <h1>หวัดดี พวก 2</h1>
     <img src="https://cdn.pixabay.com/photo/2023/07/04/10/35/sheep-8106018_1280.jpg" alt="" srcset="">
 
   </div>
-
 </template>
 
 <script>
-import { defineComponent } from 'vue'
 
-
-export default defineComponent({
-
-  name: 'App'
-});
+export default Test({
+  name: 'App2'
+})
 </script>
