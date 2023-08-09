6f4ec4af065818cc9eb1e03a485d764af74247f0
SarayutZ <wanoizloserBZ@gmail.com>
2023-08-09 14:02:00 +0700

    I want Money

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <router-view />
+  <div class="container">
+    <h1>หวัดดี พวก</h1>
+    <img src="https://cdn.pixabay.com/photo/2023/07/04/10/35/sheep-8106018_1280.jpg" alt="" srcset="">
+
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
